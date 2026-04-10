112e3a7bf6d204ef070428ab4ef0c32b59aa6c95
NaysKutzu <ghermancassian2008@gmail.com>
2026-04-10 13:12:13 +0200

    feat: add Proxmox API execution and proxy routes

--- src/routes/api/mod.rs
+++ src/routes/api/mod.rs
@@ -4,7 +4,7 @@ pub mod servers;
 pub mod system;
 
 use axum::middleware;
-use axum::{Router, routing::{get, post}};
+use axum::{Router, routing::{delete, get, post, put}};
 
 use crate::auth;
 use crate::routes::AppState;
@@ -15,6 +15,13 @@ pub fn router(state: AppState) -> Router<AppState> {
         .route("/system/stats", get(system::stats))
         .route("/proxmox/version", get(proxmox::version))
         .route("/proxmox/nodes", get(proxmox::nodes))
+        .route("/proxmox/execute", post(proxmox::execute))
+        .route("/proxmox/json/{*path}", get(proxmox::proxy_json_get))
+        .route("/proxmox/json/{*path}", post(proxmox::proxy_json_post))
+        .route("/proxmox/json/{*path}", put(proxmox::proxy_json_put))
+        .route("/proxmox/json/{*path}", delete(proxmox::proxy_json_delete))
+        .route("/proxmox/extjs/{*path}", get(proxmox::proxy_extjs_get))
+        .route("/proxmox/extjs/{*path}", post(proxmox::proxy_extjs_post))
         .route("/servers", get(servers::list_servers))
         .route("/containers", get(containers::list_containers))
         .route(
